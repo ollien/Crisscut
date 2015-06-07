e2307d39023c3ce365782a7f5c176acb0111e6fe
Nick Krichevsky <njk828@gmail.com>
2015-06-07 15:00:27 -0400

    Add correctRoutes function and some corretions for urls

--- index.js
+++ index.js
@@ -11,28 +11,27 @@ module.exports = Timpani
 
 function Timpani(routes){
 	if (routes!=undefined){
-		this.routes = routes;
+		this.routes = correctRoutes(routes);
 	}
 }
 
 Timpani.prototype.route = function(req,res,errCallback){
 	var requestUrl = url.parse(req.url).pathname; //Removes any URL arguments from the path
-	if (requestUrl[0]!='/'){
+	if (requestUrl[0]!=='/'){ //Urls should start with a /
 		requestUrl = '/'+requestUrl;
+	}
+	if (requestUrl[requestUrl.length-1]==='/' && requestUrl.length>1){ //Urls should not end in a / unless they are /
+		requestUrl = requestUrl.substring(0,requestUrl.length-1);
 	}
 	var urlSplit = requestUrl.split('/');
 	var finalRoute = null;
 	for (var i=0; i<Object.keys(this.routes).length; i++){
 		var route = Object.keys(this.routes)[i];
-		if (route[0]!='/'){
-			route = '/'+route;
-		}
-		if (route[route.length-1]==='/' && route.length>1){
-			route = route.substring(0,route.length-1);
-		}
+		
 		var routeSplit = route.split('/');
 		var argumentIndexes = matchArrayIndexes(routeSplit, ARGUMENT_PATTERN);
 		if (routeSplit.length===urlSplit.length){
+			//Browse for arguments within each url, and replace them with what they actually are in the url.
 			argumentIndexes.forEach(function(j){
 				if (urlSplit[j].length>0){
 					routeSplit[j] = urlSplit[j];	
@@ -67,4 +66,22 @@ function matchArrayIndexes(array,regexp){
 		}
 	});
 	return matchingIndexes;
+}
+
+function correctRoutes(routes){
+	Object.keys(routes).forEach(function(route){
+		var original = route;
+		var value = routes[route];
+		if (route[0]!=='/'){ //Routes must begin with a /
+			route = '/'+route;
+		}
+		if (route[route.length-1]==='/' && route.length>1){ //Routes must not end with a /, unless of course, they are to the homepage.
+			route = route.substring(0,route.length-1);
+		}
+		if (route!=original){
+			delete routes[original];
+			routes[route] = value;
+		}
+	});
+	return routes;
 }
