cef0b75f79a0659bf48c121901cd4acfef9dc46e
Chris Blake <chris@code.sc>
2015-06-19 23:52:24 -0400

    Be hostile to ollien.

--- index.js
+++ index.js
@@ -1,18 +1,68 @@
 "use strict";
 
-var _routes = {get: {}};
+var _routes = {get: {absolute: {}, relative: {}}};
 
 var router = {
 	get: function(route, fn) {
-		_routes.get[route] = fn;
+		if (route.indexOf(":") == -1) { // if the route has no ":variables"
+			_routes.get.absolute[route] = fn;
+			return this;
+		}
+		var prev = _routes.get.relative;
+		var end = {}; // should start at null?
+		var lastPath = function() {}; // default 404 handler?
+		route.split("/").reduce(function(__routes, pathSegment) {
+			if (!pathSegment) { // skip blank path segments
+				return __routes;
+			}
+			__routes[pathSegment] = {};
+			prev = __routes[pathSegment];
+			end = __routes;
+			lastPath = pathSegment;
+			return prev;
+		}, prev);
+		// set the last pathSegment match equal to the function
+		end[lastPath] = fn;
+
 		return this;
 	}
+/*
 	dispatch: function(req, res) {
-		return this;
+		var method = req.method.toLowerCase();
+		if (!method in _routes) {
+			return 404;
+		}
+		var absoluteMatch = routes[method].absolute[req.url];
+		if (absoluteMatch) {
+			return absoluteMatch(req, res, parse params);
+		}
+		then split into path segments
+		then match segment by segment in relative matches
+		else 404
 	};
+*/
 };
 
 function _routeParser(route) {
 }
 
 module.exports = router;
+
+function id() {
+	return "id";
+}
+
+router
+	.get("/user/settings", id)
+	.get("/user/:name", id)
+
+
+router.dispatch({method: "GET", url: "/user/chrisdotcode"});
+
+var util = require("util");
+console.log(util.inspect(_routes, {depth: null, colors: true}));
+
+"/user/chrisdotcode"
+
+"/user/settings"
+"/user/:user"
