52fa14e63a015d518bd15dc301f396006016b40b
Nick Krichevsky <njk828@gmail.com>
2015-06-19 23:52:36 -0400

    Modularize route function

--- index.js
+++ index.js
@@ -27,76 +27,30 @@ Crisscut.prototype.route = function(req,res,errCallback){
 	if (requestUrl[requestUrl.length-1]==='/' && requestUrl.length>1){ //Urls should not end in a / unless they are /
 		requestUrl = requestUrl.substring(0,requestUrl.length-1);
 	}
-	var urlSplit = requestUrl.split('/');
 	var finalRoute = null;
 	for (var i=0; i<Object.keys(this.routes).length; i++){
 		var route = Object.keys(this.routes)[i];
-		var routeSplit = route.split('/');
-		var argumentIndexes = matchArrayIndexes(routeSplit, ARGUMENT_PATTERN);
-		var regexMatches = null;
-		var regexDelta = 0;
-		if (routeSplit.length===urlSplit.length || arrayHasObjectWithPropertyValue(argumentIndexes,"type","regex")){
-			//Browse for arguments within each url, and replace them with what they actually are in the url.
-			for (var j = 0; j<argumentIndexes.length; j++){
-				var index = argumentIndexes[j];
-				var origIndex = index.index;
-				var offsetIndex = index.index+regexDelta;
-				if (index.type==="string" && offsetIndex<urlSplit.length){
-					if (urlSplit[offsetIndex].length>0){
-						routeSplit[origIndex] = urlSplit[offsetIndex];	
-					}
-				}
-				else if (index.type==="regex"){
-					//If ! prefixes any regex group, it should only be matched once
-					if (routeSplit[origIndex][1]!=="!"){
-						var regex = routeSplit[origIndex].substring(2,routeSplit[origIndex].length-1);	
-						var oneOnly = false;
-					}
-					else{
-						var regex = routeSplit[origIndex].substring(3,routeSplit[origIndex].length-1);
-						var oneOnly = true;
-					}
-					routeSplit[origIndex] = ""
-					//Search for regex matches in every section of the url
-					for (var l = offsetIndex; (oneOnly ? l<offsetIndex+1 && l<urlSplit.length:l<urlSplit.length); l++){
-						var match = urlSplit[l].match(regex);
-						if (	match && match[0]===urlSplit[l]){
-							routeSplit[origIndex]+=urlSplit[l];
-							if (l!=urlSplit.length-1 && !oneOnly){
-								routeSplit[origIndex]+="/";
-							}
-						}
-						else{
-							if (l!=offsetIndex){
-								regexDelta+=(l-offsetIndex)-1;
-							}
-							break;
-						}
-					}
-				}
-				if (routeSplit[origIndex][routeSplit[origIndex].length-1]==="/"){
-					routeSplit[origIndex] = routeSplit[origIndex].substring(0,routeSplit[origIndex].length-1);
-				}
-			}
-			if (routeSplit.join('/')===urlSplit.join('/')){
-				finalRoute = route;
-				break;
-			}
+		var result = createRouteSplit(route, requestUrl.split('/'));
+		var routeSplit = result.routeSplit;
+		var argumentIndexes = result.argumentIndexes;
+		if (routeSplit.join('/')===requestUrl){
+			finalRoute = route;
+			break;
 		}
 	}
 	if (finalRoute){
-		var arguments = returnArrayFromArrayIndexesObject(routeSplit,argumentIndexes);
+		var urlArguments = returnArrayFromArrayIndexesObject(routeSplit,argumentIndexes);
 		var routeMethods = this.routes[finalRoute];
 		var method = req.method.toLowerCase();
 		if (routeMethods.hasOwnProperty(method)){
-			routeMethods[method].apply(undefined,[req,res].concat(arguments));
+			routeMethods[method].apply(undefined,[req,res].concat(urlArguments));
 		}
 		else{
 			if (routeMethods.hasOwnProperty('on')){
-				routeMethods.on.apply(undefined,[req,res].concat(arguments))
+				routeMethods.on.apply(undefined,[req,res].concat(urlArguments))
 			}
 			else{
-				var pathError = JSON.parse(JSON.stringify(METHOD_NOT_ALLOWED_ERROR));
+				var pathError = clone(METHOD_NOT_ALLOWED_ERROR);
 				pathError.error = pathError.error.replace("$PATH",requestUrl);
 				pathError.error = pathError.error.replace("$METHOD",method.toUpperCase());
 				if (errCallback){
@@ -106,7 +60,7 @@ Crisscut.prototype.route = function(req,res,errCallback){
 		}
 	}
 	else{
-		var pathError = JSON.parse(JSON.stringify(PATH_NOT_FOUND_ERROR));
+		var pathError = clone(PATH_NOT_FOUND_ERROR);
 		pathError.error = pathError.error.replace("$PATH", requestUrl);	
 		if (errCallback){
 			errCallback(pathError);	
@@ -170,6 +124,75 @@ function correctRoutes(routes){
 	return routes;
 }
 
+function createRouteSplit(route,urlSplit){
+	var routeSplit = route.split('/');
+	var argumentIndexes = matchArrayIndexes(routeSplit, ARGUMENT_PATTERN);
+	var regexDelta = 0;
+	if (routeSplit.length===urlSplit.length || arrayHasObjectWithPropertyValue(argumentIndexes,"type","regex")){
+		//Browse for arguments within each url, and replace them with what they actually are in the url.
+		for (var j = 0; j<argumentIndexes.length; j++){
+			var index = argumentIndexes[j];
+			var origIndex = index.index;
+			var offsetIndex = index.index+regexDelta;
+			if (index.type==="string" && offsetIndex<urlSplit.length){
+				if (urlSplit[offsetIndex].length>0){
+					routeSplit[origIndex] = urlSplit[offsetIndex];
+				}
+			}
+			else if (index.type==="regex"){
+				var routeResult = regexArgument(clone(urlSplit), clone(routeSplit), origIndex, regexDelta);
+				urlSplit = routeResult.urlSplit;
+				routeSplit = routeResult.routeSplit;
+				regexDelta = routeResult.regexDelta;
+			}
+			if (routeSplit[origIndex][routeSplit[origIndex].length-1]==="/"){
+				routeSplit[origIndex] = routeSplit[origIndex].substring(0,routeSplit[origIndex].length-1);
+			}
+		}
+	}
+	var returnValue = {
+		routeSplit:routeSplit,
+		argumentIndexes:argumentIndexes
+	}
+	return returnValue;
+}
+
+function regexArgument(urlSplit,routeSplit,origIndex,regexDelta){
+	var offsetIndex = origIndex+regexDelta;
+	//If ! prefixes any regex group, it should only be matched once
+	if (routeSplit[origIndex][1]!=="!"){
+		var regex = routeSplit[origIndex].substring(2,routeSplit[origIndex].length-1);	
+		var oneOnly = false;
+	}
+	else{
+		var regex = routeSplit[origIndex].substring(3,routeSplit[origIndex].length-1);
+		var oneOnly = true;
+	}
+	routeSplit[origIndex] = ""
+	//Search for regex matches in every section of the url
+	for (var l = offsetIndex; (oneOnly ? l<offsetIndex+1 && l<urlSplit.length:l<urlSplit.length); l++){
+		var match = urlSplit[l].match(regex);
+		if (	match && match[0]===urlSplit[l]){
+			routeSplit[origIndex]+=urlSplit[l];
+			if (l!=urlSplit.length-1 && !oneOnly){
+				routeSplit[origIndex]+="/";
+			}
+		}
+		else{
+			if (l!=offsetIndex){
+				regexDelta+=(l-offsetIndex)-1;
+			}
+			break;
+		}
+	}
+	var returnValues = {
+		routeSplit:routeSplit,
+		urlSplit:urlSplit,
+		regexDelta:regexDelta
+	}
+	return returnValues;
+}
+
 function arrayHasObjectWithProperty(array,property){
 	for (var i = 0; i<array.length; i++){
 		if (array[i].hasOwnProperty(property)){
@@ -186,4 +209,8 @@ function arrayHasObjectWithPropertyValue(array,property,value){
 		}
 	}
 	return false;
+}
+
+function clone(object){
+	return JSON.parse(JSON.stringify(object));
 }
