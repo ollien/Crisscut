59e30924020b66960824e137411b8b1ce2154a86
Nick Krichevsky <njk828@gmail.com>
2015-06-22 23:15:09 -0400

    Refactor to use a tree structure, rather than string comparisons

--- index.js
+++ index.js
@@ -1,216 +1,302 @@
-var url = require('url');
+var url = require("url")
 
-const PATH_NOT_FOUND_ERROR = {
-	errorCode:404,
-	error:'$PATH is not a known route.'
+var routeTree = {
+	path:"/",
+	type:"explicit",
+	children:[],
+	callback:undefined
 }
-const METHOD_NOT_ALLOWED_ERROR = {
-	errorCode:405,
-	error:'$METHOD is not allowed on $PATH'
-}
-
-const ARGUMENT_PATTERN = new RegExp(":[^ \/]*");
 
 module.exports = Crisscut
 
 function Crisscut(routes){
 	if (routes!==undefined){
-		this.routes = correctRoutes(routes);
+		routes = correctRoutes(routes)
+		console.log(routes);
+		addRoutesToRouteTree(routes)	
 	}
 }
-
 Crisscut.prototype.route = function(req,res,errCallback){
-	var requestUrl = url.parse(req.url).pathname; //Removes any URL arguments from the path
-	if (requestUrl[0]!=='/'){ //Urls should start with a /
-		requestUrl = '/'+requestUrl;
-	}
-	if (requestUrl[requestUrl.length-1]==='/' && requestUrl.length>1){ //Urls should not end in a / unless they are /
-		requestUrl = requestUrl.substring(0,requestUrl.length-1);
-	}
-	var finalRoute = null;
-	for (var i=0; i<Object.keys(this.routes).length; i++){
-		var route = Object.keys(this.routes)[i];
-		var result = createRouteSplit(route, requestUrl.split('/'));
-		var routeSplit = result.routeSplit;
-		var argumentIndexes = result.argumentIndexes;
-		if (routeSplit.join('/')===requestUrl){
-			finalRoute = route;
-			break;
-		}
-	}
-	if (finalRoute){
-		var urlArguments = returnArrayFromArrayIndexesObject(routeSplit,argumentIndexes);
-		var routeMethods = this.routes[finalRoute];
+	var parsedUrl = url.parse(req.url)
+	var rawUrl = parsedUrl.pathname
+	var rawArguments = parsedUrl.query
+	var routeResult = findRouteFromUrl(rawUrl);
+	if (routeResult!=null){
+		var methods = routeResult.methods;
+		var args = routeResult.args;
 		var method = req.method.toLowerCase();
-		if (routeMethods.hasOwnProperty(method)){
-			routeMethods[method].apply({},[req,res].concat(urlArguments));
+		var parsedUrlArgs = parseArguments(rawArguments);
+		console.log(rawArguments);
+		console.log(parsedUrlArgs);
+		if (methods.hasOwnProperty(method)){
+			methods[method].apply({},[req,res].concat(args,parsedUrlArgs))
+		}
+		else if (methods.hasOwnProperty("on")){
+			methods.on.apply({},[req,res].concat(args,parsedUrlArgs))
 		}
 		else{
-			if (routeMethods.hasOwnProperty('on')){
-				routeMethods.on.apply({},[req,res].concat(urlArguments))
-			}
-			else{
-				var pathError = clone(METHOD_NOT_ALLOWED_ERROR);
-				pathError.error = pathError.error.replace("$PATH",requestUrl);
-				pathError.error = pathError.error.replace("$METHOD",method.toUpperCase());
-				if (errCallback){
-					errCallback(pathError);
-				}
-			}
+			errCallback(methodNotAllowed(rawUrl,method));
 		}
 	}
 	else{
-		var pathError = clone(PATH_NOT_FOUND_ERROR);
-		pathError.error = pathError.error.replace("$PATH", requestUrl);	
-		if (errCallback){
-			errCallback(pathError);	
-		}
+		errCallback(pathNotFound(rawUrl))
 	}
-}
-
-Crisscut.prototype.addRoute = function(method,route,routeCallback){
-	method = method.toLowerCase();
-	route = route.toLowerCase();
-	if (!this.routes.hasOwnProperty(route)){
-		this.routes[route] = {}
-	}
-	this.routes[route][method] = routeCallback;
-}
-
-function matchArrayIndexes(array,regexp){
-	var matchingIndexes = []
-	array.forEach(function(element,index){
-		if ((element[1]==="(" || element[2]==="(") && element[element.length-1]===")"){
-			matchingIndexes.push({index:index,type:"regex"})
-		}
-		else if (element.match(regexp)){
-			matchingIndexes.push({index:index,type:"string"});
-		}
-		
-	});
-	return matchingIndexes;
-}
-
-function returnArrayFromArrayIndexesObject(array,indexes){
-	var items = [];
-	indexes.forEach(function(item){
-		items.push(array[item.index]);
-	});
-	return items;
 }
 
 function correctRoutes(routes){
 	Object.keys(routes).forEach(function(route){
-		var original = route;
-		var value = routes[route];
-		if (route[0]!=='/'){ //Routes must begin with a /
-			route = '/'+route;
+		var original = route
+		var value = routes[route]
+		if (route[0]==="/" && route.length>1){ //Routes must not begin with a /, unless they are /
+			route = route.substring(1)
 		}
-		if (route[route.length-1]==='/' && route.length>1){ //Routes must not end with a /, unless of course, they are to the homepage.
-			route = route.substring(0,route.length-1);
+		if (route[route.length-1]==="/" && route.length>1){ //Routes must not end with a /, unless of course, they are to the homepage.
+			route = route.substring(0,route.length-1)
 		}
 		if (route!=original){
-			delete routes[original];
-			routes[route] = value;
+			delete routes[original]
+			routes[route] = value
 		}
-		Object.keys(routes[route]).forEach(function(item){
-			if (item!=item.toLowerCase()){
-				var value = routes[route][item];
-				delete routes[route][item];
-				routes[route][item.toLowerCase()] = value;
-			}
-		})
-	});
-	return routes;
-}
-
-function createRouteSplit(route,urlSplit){
-	var routeSplit = route.split('/');
-	var argumentIndexes = matchArrayIndexes(routeSplit, ARGUMENT_PATTERN);
-	var regexDelta = 0;
-	if (routeSplit.length===urlSplit.length || arrayHasObjectWithPropertyValue(argumentIndexes,"type","regex")){
-		//Browse for arguments within each url, and replace them with what they actually are in the url.
-		for (var j = 0; j<argumentIndexes.length; j++){
-			var index = argumentIndexes[j];
-			var origIndex = index.index;
-			var offsetIndex = index.index+regexDelta;
-			if (index.type==="string" && offsetIndex<urlSplit.length){
-				if (urlSplit[offsetIndex].length>0){
-					routeSplit[origIndex] = urlSplit[offsetIndex];
+		if (typeof routes[route] === "object"){
+			Object.keys(routes[route]).forEach(function(item){
+				if (item!=item.toLowerCase()){
+					var value = routes[route][item]
+					delete routes[route][item]
+					routes[route][item.toLowerCase()] = value
 				}
-			}
-			else if (index.type==="regex"){
-				var routeResult = regexArgument(clone(urlSplit), clone(routeSplit), origIndex, regexDelta);
-				urlSplit = routeResult.urlSplit;
-				routeSplit = routeResult.routeSplit;
-				regexDelta = routeResult.regexDelta;
-			}
-			if (routeSplit[origIndex][routeSplit[origIndex].length-1]==="/"){
-				routeSplit[origIndex] = routeSplit[origIndex].substring(0,routeSplit[origIndex].length-1);
-			}
+			})
 		}
-	}
-	var returnValue = {
-		routeSplit:routeSplit,
-		argumentIndexes:argumentIndexes
-	}
-	return returnValue;
+	})
+	return routes
 }
 
-function regexArgument(urlSplit,routeSplit,origIndex,regexDelta){
-	var offsetIndex = origIndex+regexDelta;
-	//If ! prefixes any regex group, it should only be matched once
-	if (routeSplit[origIndex][1]!=="!"){
-		var regex = routeSplit[origIndex].substring(2,routeSplit[origIndex].length-1);	
-		var oneOnly = false;
+
+function addRouteToRouteTree(route,functions,parentNode){
+	if (route.length===0){
+		return; //No point in adding a route that literally is nothing.
+	}
+	if (route==="/"){ // We already have a root element pre-made, we can just add a function in
+		routeTree.functions = functions
+		return; 
+	}
+	var routeSplit = route.split("/")
+	if (parentNode===undefined || parentNode===null){
+		parentNode = routeTree
+	}
+	var index = findObjectWithPropertyValueInArray(parentNode.children, "path",routeSplit[0])
+	if (index>-1){
+		routeSplit.shift()
+		addRouteToRouteTree(routeSplit.join("/"), functions, parentNode.children[index])
 	}
 	else{
-		var regex = routeSplit[origIndex].substring(3,routeSplit[origIndex].length-1);
-		var oneOnly = true;
-	}
-	routeSplit[origIndex] = ""
-	//Search for regex matches in every section of the url
-	for (var l = offsetIndex; (oneOnly ? l<offsetIndex+1 && l<urlSplit.length:l<urlSplit.length); l++){
-		var match = urlSplit[l].match(regex);
-		if (	match && match[0]===urlSplit[l]){
-			routeSplit[origIndex]+=urlSplit[l];
-			if (l!=urlSplit.length-1 && !oneOnly){
-				routeSplit[origIndex]+="/";
+		var type = routeSplit[0][0]===":" ? "variable":"explicit"
+		if (type==="variable"){
+			type = routeSplit[0][1]==="(" && routeSplit[0][routeSplit[0].length-1]===")" ? "regex":"variable"
+		}
+		if (type==="variable"){
+			index = findObjectWithPropertyValueInArray(parentNode.children, "type", "variable")
+			if (index>-1){
+				routeSplit.shift()
+				addRouteToRouteTree(routeSplit.join("/"), functions, routeSplit.children[index])
+			}
+			else{
+				var leaf = createLeaf(routeSplit[0], type)
+				parentNode.children.push(leaf)
+				routeSplit.shift()
+				if (routeSplit.length>0){
+					addRouteToRouteTree(routeSplit.join("/"), functions, leaf)
+				}
+				else{
+					leaf.functions = functions
+				}
 			}
 		}
 		else{
-			if (l!=offsetIndex){
-				regexDelta+=(l-offsetIndex)-1;
+			var leaf = createLeaf(routeSplit[0], type)
+			parentNode.children.push(leaf)
+			routeSplit.shift()
+			if (routeSplit.length>0){
+				addRouteToRouteTree(routeSplit.join("/"), functions, leaf)
 			}
-			break;
+			else{
+				leaf.functions = functions
+			}
 		}
 	}
-	var returnValues = {
-		routeSplit:routeSplit,
-		urlSplit:urlSplit,
-		regexDelta:regexDelta
-	}
-	return returnValues;
 }
 
+function addRoutesToRouteTree(routes){
+	Object.keys(routes).forEach(function(route){
+		addRouteToRouteTree(route,routes[route])
+	})
+}
+
+function findRouteFromUrl(url,parentNode,args){
+	if (parentNode===null || parentNode===undefined){
+		parentNode = routeTree
+	}
+	if (args===null || args===undefined){
+		args = []
+	}
+	if (url==="/"){
+		if (parentNode.functions==null){
+			return null;
+		}
+		else{
+			return {
+				methods:parentNode.functions,
+				args:args
+			}
+		}
+	}
+	else{
+		if (url[0]==="/"){
+			url = url.substring(1);	
+		} 
+		if (url[url.length-1]==="/"){
+			url = url.substring(0,url.length-1)
+		}
+		
+	}
+	var urlSplit = url.split("/")
+	var index = findObjectWithPropertyValueInArray(parentNode.children, "path", urlSplit[0])
+	//If we have an explicit route, use it, otherwise, we need to do some searching.
+	if (index>-1){
+		urlSplit.shift()
+		if (urlSplit.length>0){
+			return findRouteFromUrl(urlSplit.join("/"), parentNode.children[index],args)
+		}
+		else{
+			var methods = parentNode.children[index].functions
+			if (methods===null || methods===undefined){
+				return null
+			}
+
+			return {
+				methods:methods,
+				args:args
+			}
+		}
+	}
+	else{
+		var regexIndexes = findObjectsWithPropertyValueInArray(parentNode.children, "type", "regex")
+		var variableIndex = findObjectWithPropertyValueInArray(parentNode.children, "type", "variable") //There should only ever be one variable in the tree.
+		for (var i=0; i<regexIndexes.length; i++){
+			var index = regexIndexes[i];
+			var match = urlSplit[0].match(parentNode.children[index].path.substring(1)); 
+			if (match && match[0]===urlSplit[0]){
+				args.push(urlSplit[0])
+				urlSplit.shift()
+				if (urlSplit.length>0){
+					return findRouteFromUrl(urlSplit.join("/"), parentNode.children[index],args)
+				}
+				else{
+					var methods = parentNode.children[index].functions
+					if (methods===null || methods===undefined){
+						return null
+					}
+
+					return {
+						methods:methods,
+						args:args
+					}
+				}
+			}
+		}
+		//If we haven't matched regex, we can see if we have a variable. otherwise, we return null.
+		if (variableIndex>-1){
+			args.push(urlSplit[0])
+			urlSplit.shift()
+			if (urlSplit.length>0){
+				return findRouteFromUrl(urlSplit.join("/"),parentNode.chldren[variableIndex],args)
+			}
+			else{
+				var methods = parentNode.children[index].functions
+				if (methods===null || methods===undefined){
+					return null
+				}
+
+				return {
+					methods:methods,
+					args:args
+				}
+			}
+		}	
+	}
+}
+
+function createLeaf(name,type,func){
+	return {
+		path:name,
+		type:type,
+		children:[],
+		func:func,
+	}
+}
+
+function parseArguments(queryString){
+	if (queryString[0]==="?"){
+		queryString = queryString.substring(1)
+	}
+	var andSplit = queryString.split("&")
+	var parsedArgs = {}
+	andSplit.forEach(function(item){
+		var split = item.split("=")
+		parsedArgs[split[0]]=split[1]
+	});
+	return parsedArgs
+}
+
+function pathNotFound(path){
+	return {
+		errorCode:404,
+		error:path+" is not a known route."
+	}
+}
+function methodNotAllowed(path,method){
+	return {
+		errorCode:405,
+		error: method+" is not allowed on "+path
+	}
+
+}
 function arrayHasObjectWithProperty(array,property){
 	for (var i = 0; i<array.length; i++){
 		if (array[i].hasOwnProperty(property)){
-			return true;
+			return true
 		}
 	}
-	return false;
+	return false
 }
 
 function arrayHasObjectWithPropertyValue(array,property,value){
 	for (var i = 0; i<array.length; i++){
 		if (array[i].hasOwnProperty(property) && array[i][property]===value){
-			return true;
+			return false
 		}
 	}
-	return false;
+	return true
+}
+
+function findObjectWithPropertyValueInArray(array,property,value){
+	for (var i = 0; i<array.length; i++){
+		if (array[i].hasOwnProperty(property) && array[i][property]===value){
+			return i
+		}
+	}
+	return -1
+}
+
+function findObjectsWithPropertyValueInArray(array,property,value){
+	var results = []
+	for (var i = 0; i<array.length; i++){
+		if (array[i].hasOwnProperty(property) && array[i][property]===value){
+			results.push(i)
+		}
+	}
+	return results
 }
 
 function clone(object){
-	return JSON.parse(JSON.stringify(object));
+	return JSON.parse(JSON.stringify(object))
 }
